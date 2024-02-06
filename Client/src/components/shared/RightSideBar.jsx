91d14ffb10744ac8781e68cb3cb50be24a787b50
Shincy <shincyshnz@gmail.com>
2024-02-06 23:12:11 +0400

    display Suggested users detail

--- Client/src/components/shared/RightSideBar.jsx
+++ Client/src/components/shared/RightSideBar.jsx
@@ -9,7 +9,7 @@ export const ProfileCard = ({ ...props }) => {
     <div className="flex flex-col items-center justify-center mt-3">
       <div className="w-full flex items-center gap-3">
         <a href='#'>
-          <img className="w-12 h-12 p-[2px] rounded-full" src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+          <img className="w-12 h-12 p-[2px] rounded-full" src={imgUrl || "https://flowbite.com/docs/images/carousel/carousel-1.svg"} alt="profile picture" />
         </a>
         <div className="flex flex-1 flex-col items-start justify-center">
           <span className='text-md font-bold'>{username}</span>
@@ -43,14 +43,15 @@ const RightSideBar = () => {
       const error = suggestedUserError || profileError;
       handleError('suggestedUsersApiError', { message: error?.response?.data?.message || error?.message });
     }
-  }, [suggestedUserError, profileError, handleError]);
+    console.log(suggestedUsers);
+  }, [suggestedUserError, profileError]);
 
   // Markup
   return (
     <div className="w-full px-3 md:px-8 mt-6 max-w-[380px] text-center hidden lg:block">
       {isPendingProfile ? <Loader /> : (
         <>
-          <ProfileCard imgUrl="assets/cake.png" username={profile.username} subText={profile.fullname} text="Switch" />
+          <ProfileCard imgUrl={profile.profile_pic} username={profile.username} subText={profile.fullname} text="Switch" />
 
           <div className="flex items-center  font-bold text-[14px]">
             <h5 className='flex-1 my-5 text-left text-gray-600'>Suggested for you</h5>
@@ -59,7 +60,9 @@ const RightSideBar = () => {
 
 
           <div className="flex flex-col">
-            <ProfileCard imgUrl="assets/cake.png" username="shincy_raffy" subText="Follows You." />
+            {suggestedUsers.map((user, i) => (
+              <ProfileCard imgUrl={user.profile_pic} username={user.username} subText="Follows You." />
+            ))}
             <ProfileCard imgUrl="assets/cake.png" username="shincy_raffy" subText="Follows You." />
             <ProfileCard imgUrl="assets/cake.png" username="shincy_raffy" subText="Suggested for you." />
             <ProfileCard imgUrl="assets/cake.png" username="shincy_raffy" subText="Suggested for you." />
