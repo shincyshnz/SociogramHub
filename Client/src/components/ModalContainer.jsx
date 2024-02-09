067cbd36f000708013e238d2816a695fd6f7bd5e
Shincy <shincyshnz@gmail.com>
2024-02-09 22:33:17 +0400

    Modal container created. task for tomorrow :: Impelement HOC for modal container

--- Client/src/components/ModalContainer.jsx
+++ Client/src/components/ModalContainer.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 const ModalContainer = () => {
     return (
-        <div className='fixed flex-center w-full max-h-screen px-10 md:px-0 bg-slate-900 inset-0 z-40 overflow-hidden'>
-            <div className='w-full h-64 md:w-80 bg-white rounded-lg p-3'>
-                
+        <div className='absolute w-full flex-center bg-opacity-60 max-h-screen px-10 md:px-0 bg-slate-900 inset-0 z-40 overflow-hidden'>
+            <div className='fixed w-full h-64 md:w-80 bg-white rounded-lg p-3'>
             </div>
         </div>
     )
--- Client/src/components/ProfileCard.jsx
+++ Client/src/components/ProfileCard.jsx
@@ -1,12 +1,15 @@
-import { Spinner } from 'flowbite-react';
+import { useState } from 'react';
 import { useError } from '../hooks/customHooks';
 import { useFollowUsers } from '../lib/reactQuery/queriesAndMutations';
+import { Spinner } from 'flowbite-react';
+import ModalContainer from './ModalContainer';
 
 const ProfileCard = ({ ...props }) => {
     const { userId, imgUrl, username, subText, text = "follow" } = props;
     const { handleError } = useError();
+    const [isOpenModal, setIsOpenModal] = useState(false);
 
-
+    const TEMP_PROFILE_PIC_URL = "https://flowbite.com/docs/images/carousel/carousel-1.svg";
     // Follow User
     const {
         mutateAsync: followUser,
@@ -28,30 +31,45 @@ const ProfileCard = ({ ...props }) => {
         event.preventDefault();
 
         // Load the unfollow confirmation modal
-        console.log("unfollow");
+        setIsOpenModal(true);
     }
 
     return (
-        <div className="flex flex-col items-center justify-center mt-3">
-            <div className="w-full flex items-center gap-3">
-                <img className="w-12 h-12 p-[2px] rounded-full" src={imgUrl || "https://flowbite.com/docs/images/carousel/carousel-1.svg"} alt="profile picture" />
-                <div className="flex flex-1 flex-col items-start justify-center">
-                    <span className='text-md font-bold'>{username}</span>
-                    <span className=' text-gray-600'>{subText}</span>
-                </div>
-                {isFollowSuccess
-                    ? <button onClick={handleUnFollowLink}>
-                        <span className='font-bold'>Following</span>
-                    </button>
-                    : (isPendingFollowing
-                        ? <Spinner />
-                        : <button onClick={handleFollowLink}>
-                            <span className='text-blue-700 font-bold'>{text}</span>
+        <>
+            <div className="flex flex-col items-center justify-center mt-3">
+                <div className="w-full flex items-center gap-3">
+                    <img className="w-12 h-12 p-[2px] rounded-full" src={imgUrl || TEMP_PROFILE_PIC_URL} alt="profile picture" />
+                    <div className="flex flex-1 flex-col items-start justify-center">
+                        <span className='text-md font-bold'>{username}</span>
+                        <span className=' text-gray-600'>{subText}</span>
+                    </div>
+                    {isFollowSuccess
+                        ? <button onClick={handleUnFollowLink}>
+                            <span className='font-bold'>Following</span>
                         </button>
-                    )
-                }
+                        : (isPendingFollowing
+                            ? <Spinner />
+                            : <button onClick={handleFollowLink}>
+                                <span className='text-blue-700 font-bold'>{text}</span>
+                            </button>
+                        )
+                    }
+                </div>
             </div>
-        </div>
+
+            {/* Modal :  Implement HOC : Higher Order Component */}
+           
+            {
+                // isOpenModal &&
+                // <ModalContainer >
+                //     <img className="w-12 h-12 p-[2px] rounded-full" src={imgUrl || TEMP_PROFILE_PIC_URL} alt="profile picture" />
+                //     <div className="flex flex-1 flex-col items-start justify-center">
+                //         <span className='text-md font-bold'>{username}</span>
+                //         <span className=' text-gray-600'>{subText}</span>
+                //     </div>
+                // </ModalContainer>
+            }
+        </>
     );
 }
 
