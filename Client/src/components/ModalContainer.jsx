fae16c4af238e401fc82e41168610fa3f0723163
Shincy <shincyshnz@gmail.com>
2024-02-21 11:03:08 +0400

    making modal container single div

--- Client/src/components/ModalContainer.jsx
+++ Client/src/components/ModalContainer.jsx
@@ -5,9 +5,7 @@ const ModalContainer = ({ children, modalContainerClassName }) => {
         // <div className="fixed top-8 bg-white max-h-[480px] w-3/4 lg:w-1/2 mx-auto p-2 overflow-x-hidden overflow-y-auto inset-0 z-40 outline-none focus:outline-none rounded-lg">
 
         <div className={modalContainerClassName}>
-            <div className='fixed w-full h-64 md:w-80 bg-white rounded-lg p-3'>
-                {children}
-            </div>
+            {children}
         </div>
     )
 }
--- Client/src/components/UnfollowModal.jsx
+++ Client/src/components/UnfollowModal.jsx
@@ -3,14 +3,15 @@ import WithModal from "./ModalContainer";
 // Use HOC Modal Container
 const UnfollowModalBody = ({ imgUrl, username, handleUnFollow, setIsOpenModal, modalContainerClassName }) => {
     return (
-        <>
+        <div className='fixed w-full h-64 md:w-80 bg-white rounded-lg p-3'>
             <div className="flex-center flex-col text-md">
                 <img className="w-12 h-12 p-[2px] rounded-full" src={imgUrl || import.meta.env.VITE_TEMP_PROFILE_PIC_URL} alt="profile picture" />
                 <p className='p-3'>{`Unfollow ${username} ?`}</p>
                 <button className='unfollow-modal-btn text-red-600' onClick={handleUnFollow}>Unfollow</button>
                 <button className='unfollow-modal-btn' onClick={() => setIsOpenModal(false)}>Cancel</button>
             </div>
-        </>)
+        </div>
+    )
 }
 
 export default WithModal(UnfollowModalBody);
